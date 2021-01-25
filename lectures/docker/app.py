fbcef71d6ed791094406843e626347b47d1cfe8f
Andrii <plug2up@gmail.com>
2021-01-25 21:24:41 +0200

    HW#5 Done

--- lectures/docker/app.py
+++ lectures/docker/app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
 
-@app.route('/hello')
+@app.route('/')
 def hello():
-    return 'Hi there!'
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
